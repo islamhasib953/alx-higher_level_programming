08f4c93638039848ae78ead025a4553d7861625d
islamhasib953 <islamhasib953@gmail.com>
2023-11-28 20:33:23 +0200

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(100):
+    if i != 99:
+        print("{:s}".format(str(i).rjust(2,'0')), end=", ")
+    else:
+        print("{:s}".format(str(i).rjust(2,'0')))
+print("\n")
