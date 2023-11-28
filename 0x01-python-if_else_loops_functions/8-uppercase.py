ca82836079662b2ed1ac6e0a581d2032fc1afd0e
islamhasib953 <islamhasib953@gmail.com>
2023-11-28 21:17:12 +0200

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,6 @@ def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             str = str.replace(i, chr(ord(i)-32))
-    print(str)
-
-
+    print("{}".format(str))
 uppercase("best")
 uppercase("Best School 98 Battery street")
