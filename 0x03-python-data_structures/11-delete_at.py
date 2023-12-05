4d023b2780d0a962a4d7d8c940080b4233432e4b
islamhasib953 <islamhasib953@gmail.com>
2023-12-05 22:15:30 +0200

    0x03-python-data_structures

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def delet(my_list=[], idx=0):
-    if idx < len(my_list):
-        my_list.pop(idx)
-    return my_list
-
-
 def delete_at(my_list=[], idx=0):
-    return delet(my_list, idx)
+    if idx >= len(my_list) or idx < 0:
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
