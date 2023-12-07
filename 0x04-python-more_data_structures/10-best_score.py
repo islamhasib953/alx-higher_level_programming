78af6cfc2fc374a8818774d011eac4b27b8b74cd
islamhasib953 <islamhasib953@gmail.com>
2023-12-07 16:49:49 +0200

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary and len(a_dictionary):
-        mx = list(a_dictionary.key())[0]
+        mx = list(a_dictionary.keys())[0]
         for i in a_dictionary:
             if a_dictionary[i] > a_dictionary[mx]:
                 mx = i
