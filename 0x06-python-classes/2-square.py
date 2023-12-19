a303231920adbf3609b48fff90b87d58a37394d0
islamhasib953 <islamhasib953@gmail.com>
2023-12-19 22:36:20 +0200

    0x06-python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,8 @@ class Square:
     """
     square class
     """
-    def __init__(self, size = 0):
+
+    def __init__(self, size=0):
         """
         constractor for square class
         Args:
