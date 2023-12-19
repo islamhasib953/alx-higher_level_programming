5f465ef27531358d2e8f60ecc6cdb1f9af3ab32b
islamhasib953 <islamhasib953@gmail.com>
2023-12-19 22:35:42 +0200

    0x06-python-classes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+creates a square class object
+"""
+
+
+class Square:
+    """
+    square class
+    """
+    def __init__(self, size = 0):
+        """
+        constractor for square class
+        Args:
+        size (int) : size of sauare, defualt  = 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("ize must be >= 0")
+        self.__size = size
