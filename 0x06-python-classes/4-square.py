3b8b6028e924b9f48c2a1c6f26a86463ae666141
islamhasib953 <islamhasib953@gmail.com>
2023-12-19 23:11:03 +0200

    0x05-python-exceptions

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-"""
-creates a square class object
-"""
+"""classes and objects tasks."""
 
 
 class Square:
-    """
-    square class
-    """
+    """square class."""
+
     def __init__(self, size=0):
         """
-        constractor for square class
+        Initialise Constructor for Square class.
+
         Args:
-        size (int) : size of sauare, defualts to zero
+        size (int) : the size of the square. defaults to Zero.
         """
         self.__size = size
+
     @property
     def size(self):
-        """size getter"""
+        """Size getter."""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
-        """size setter"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("ize must be >= 0")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
