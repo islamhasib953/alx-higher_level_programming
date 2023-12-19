5aeea5f0551425589312c47a5898d0071874011b
islamhasib953 <islamhasib953@gmail.com>
2023-12-19 22:28:59 +0200

    0x06-python-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,10 +5,13 @@ creates a square class object
 
 
 class Square:
+    """
+    square class
+    """
     def __init__(self, size):
         """
         constractor for square class
         Args:
-        size(int): size of sauare
+        size (int) : size of sauare
         """
         self.__size = size
