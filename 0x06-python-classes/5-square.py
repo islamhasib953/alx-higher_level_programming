36ff5f4656105481c4d6ccffcb3f00a8730f028d
islamhasib953 <islamhasib953@gmail.com>
2023-12-19 23:04:51 +0200

    0x05-python-exceptions

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+creates a square class object
+"""
+
+
+class Square:
+    """
+    square class
+    """
+
+    def __init__(self, size=0):
+        """
+        constractor for square class
+        Args:
+        size (int) : size of sauare, defualts to zero
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("ize must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        returns the current square area
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
