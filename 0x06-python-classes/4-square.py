e344e56b72f5a185d1b592c7af513d91f7815960
islamhasib953 <islamhasib953@gmail.com>
2023-12-19 23:00:48 +0200

    0x05-python-exceptions

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,24 +8,21 @@ class Square:
     """
     square class
     """
-
     def __init__(self, size=0):
         """
         constractor for square class
         Args:
         size (int) : size of sauare, defualts to zero
         """
+        self.__size = size
     @property
     def size(self):
-        """
-        size getter
-        """
+        """size getter"""
         return self.__size
+    
     @size.setter
     def size(self, value):
-        """
-        size setter
-        """
+        """size setter"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
