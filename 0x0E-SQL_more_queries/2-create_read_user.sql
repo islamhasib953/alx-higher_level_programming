03abc13063af6b4c1438f56be90bcdbf2871a6cb
islamhasib953 <islamhasib953@gmail.com>
2024-02-15 15:23:59 +0200

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+GRANT SELECT ON *.* TO `user_0d_1`@`localhost`
