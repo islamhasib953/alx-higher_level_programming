87fbe7f1ed6f613a197eb0aa22a43baa1ae3824b
islamhasib953 <islamhasib953@gmail.com>
2024-03-12 00:01:06 +0200

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+if (!num) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    let row = '';
+    for (let j = 0; j < num; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
