2e833bca3598b1e5e030e055a91c79e0c03f25f1
islamhasib953 <islamhasib953@gmail.com>
2024-03-11 23:50:36 +0200

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < 3; i++) {
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
