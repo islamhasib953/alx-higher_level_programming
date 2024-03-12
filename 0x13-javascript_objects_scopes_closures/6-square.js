c38706ecde89f56166dd287ad50711110abc88e5
islamhasib953 <islamhasib953@gmail.com>
2024-03-12 23:48:57 +0200

    0x12-javascript-warm_up

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
+const BaseSquare = require('./5-square');
 
+class Square extends BaseSquare {
   charPrint (c) {
     if (c === undefined) { c = 'X'; }
     for (let i = 0; i < this.height; i++) {
