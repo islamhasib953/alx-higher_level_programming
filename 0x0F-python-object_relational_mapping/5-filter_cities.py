049552100349a74eaceea1ab44d8f691e199b685
islamhasib953 <islamhasib953@gmail.com>
2024-03-17 00:54:25 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,10 +1,53 @@
+# #!/usr/bin/python3
+
+# """
+# 5-filter_cities.py:
+# script that takes in the name of a state as an argument
+# and lists all cities of that state, using the database hbtn_0e_4_usa
+# Usage: ./5-filter_cities.py <mysql username> <mysql password> <database name>
+# """
+
+# import MySQLdb
+# import sys
+
+# if __name__ == "__main__":
+#     db = MySQLdb.connect(
+#         host="localhost",
+#         port=3306,
+#         user=sys.argv[1],
+#         passwd=sys.argv[2],
+#         db=sys.argv[3],
+#     )
+
+#     cusrsor = db.cursor()
+#     name = sys.argv[4]
+#     cusrsor.execute(
+#         """
+#     SELECT c.name
+#                     FROM cities c
+#                     JOIN  states s
+#                     ON c.state_id=s.id
+#                     WHERE s.name = %s
+#                 ORDER BY c.id
+#     """,
+#         (name,),
+#     )
+
+#     for row in cusrsor.fetchall():
+#         print(row)
+
+#     cusrsor.close()
+#     db.close()
+
+
 #!/usr/bin/python3
 
 """
-5-filter_cities.py:
-script that takes in the name of a state as an argument
-and lists all cities of that state, using the database hbtn_0e_4_usa
-Usage: ./5-filter_cities.py <mysql username> <mysql password> <database name>
+3-my_safe_filter_states.py:
+script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+Usage: ./3-my_safe_filter_states.py <mysql username> <mysql password>
+<database name> <state name searched>
 """
 
 import MySQLdb
@@ -18,23 +61,18 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3],
     )
-
-    cusrsor = db.cursor()
+    cur = db.cursor()
     name = sys.argv[4]
-    cusrsor.execute(
-        """
-    SELECT c.name
-                    FROM cities c
-                    JOIN  states s
-                    ON c.state_id=s.id
-                    WHERE s.name = %s
-                ORDER BY c.id
-    """,
+    cur.execute(
+        "SELECT `cities`.`name`\
+                FROM `cities`\
+                JOIN `states` ON `cities`.`state_id` = `states`.`id`\
+                WHERE `states`.`name` = %s\
+                ORDER BY `cities`.`id`",
         (name,),
     )
 
-    for row in cusrsor.fetchall():
-        print(row)
-
-    cusrsor.close()
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
+    cur.close()
     db.close()
