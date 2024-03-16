18f020fbce678683980fd9fcea4b88fc936a42fc
islamhasib953 <islamhasib953@gmail.com>
2024-03-16 23:44:24 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,8 @@
 2-my_filter_states.py:
 script that takes in an argument and displays all values in the states table of
 hbtn_0e_0_usa where name matches the argument.
-Usage: ./2-my_filter_states.py <mysql username> <mysql password> <database name> <search name>
+Usage: ./2-my_filter_states.py
+<mysql username> <mysql password> <database name> <search name>
 """
 
 import MySQLdb
@@ -18,11 +19,10 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        charset="utf8"
     )
 
     cursor = db.cursor()
-    cursor.execute(f"SELECT * FROM `states` WHERE `name`='{sys.argv[4]}'")
+    cursor.execute("SELECT * FROM `states` WHERE `name`='{}'".format(sys.argv[4]))
     for row in cursor.fetchall():
         print(row)
     cursor.close()
