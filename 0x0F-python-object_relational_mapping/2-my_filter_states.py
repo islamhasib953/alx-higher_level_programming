382562882525a7a11e3d85bff252ebcc6f8e4ab8
islamhasib953 <islamhasib953@gmail.com>
2024-03-16 23:42:16 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 """
-1-filter_states.py:
-script that lists all states with a name starting with N
-(upper N) from the database hbtn_0e_0_usa
-Usage: ./1-filter_states.py <mysql username> <mysql password> <database name>
+2-my_filter_states.py:
+script that takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+Usage: ./2-my_filter_states.py <mysql username> <mysql password> <database name> <search name>
 """
 
 import MySQLdb
