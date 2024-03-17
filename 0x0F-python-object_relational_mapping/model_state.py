b8f155cd31f3ea2c7c605c8a2f71849d037c2c10
islamhasib953 <islamhasib953@gmail.com>
2024-03-17 11:33:00 +0200

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+First state model
+"""
+
+from sqlalchemy import create_engine, Column, Integer, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Class State inherits from Base
+    """
+
+    __tablename__ = "states"
+    id = Column("id", Integer(11), primary_key=True, autoincrement=True, nullable=False)
+    name = Column("name", VARCHAR(128), nullable=False)
