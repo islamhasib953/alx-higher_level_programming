da7f9154b9a7496266e7872a108c5a4dff875a0f
islamhasib953 <islamhasib953@gmail.com>
2024-03-17 11:40:40 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,42 @@
+# #!/usr/bin/python3
+
+# """
+# First state model
+# """
+
+# from sqlalchemy import create_engine, Column, Integer, VARCHAR
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# Base = declarative_base()
+
+
+# class State(Base):
+#     """
+#     Class State inherits from Base
+#     """
+
+#     __tablename__ = "states"
+#     id = Column(Integer(11), primary_key=True, autoincrement=True, nullable=False)
+#     name = Column(VARCHAR(128), nullable=False)
+
+
 #!/usr/bin/python3
-
 """
-First state model
+base srate model class for the project
 """
 
-from sqlalchemy import create_engine, Column, Integer, VARCHAR
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, VARCHAR, Column, Integer, String
 
 Base = declarative_base()
 
 
 class State(Base):
     """
-    Class State inherits from Base
+    State class
     """
 
     __tablename__ = "states"
-    id = Column(Integer(11), primary_key=True, autoincrement=True, nullable=False)
-    name = Column(VARCHAR(128), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
