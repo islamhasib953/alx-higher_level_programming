7b397070dcd10e0eb3f171fcc121d1331b4f8beb
islamhasib953 <islamhasib953@gmail.com>
2024-03-17 11:43:42 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,42 +1,21 @@
-# #!/usr/bin/python3
-
-# """
-# First state model
-# """
-
-# from sqlalchemy import create_engine, Column, Integer, VARCHAR
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-
-# Base = declarative_base()
-
-
-# class State(Base):
-#     """
-#     Class State inherits from Base
-#     """
-
-#     __tablename__ = "states"
-#     id = Column(Integer(11), primary_key=True, autoincrement=True, nullable=False)
-#     name = Column(VARCHAR(128), nullable=False)
-
-
 #!/usr/bin/python3
+
 """
-base srate model class for the project
+First state model
 """
 
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import create_engine, VARCHAR, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 
 class State(Base):
     """
-    State class
+    Class State inherits from Base
     """
 
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+    name = Column(VARCHAR(128), nullable=False)
