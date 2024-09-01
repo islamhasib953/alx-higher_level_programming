541d8f428f0b8cefffc7239f074a723334a8d3c3
islamhasib953 <islamhasib953@example.com>
2024-09-01 20:57:30 +0300

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""2-append_write"""
+
+
+def append_write(filename="", text=""):
+    """appends a string at the end of a text file"""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        f.write(text)
