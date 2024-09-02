29fceed818fb0512e2bbb141685d1e421ac86d8a
islamhasib953 <islamhasib953@example.com>
2024-09-02 15:52:59 +0300

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,5 +83,5 @@ class Rectangle(Base):
     def __str__(self):
         """return metgod str"""
         return "[Rectangle] ( {} ) {} / {} - {} / {}".format(
-            self.id, self.__x, self.__y, self.__width, self.__height
+            self.id, self.x, self.y, self.width, self.height
         )
