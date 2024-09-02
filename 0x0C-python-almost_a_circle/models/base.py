db9af202b3d7356d18fa32072c9c05c14975a0f5
islamhasib953 <islamhasib953@example.com>
2024-09-02 20:00:38 +0300

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,3 +48,8 @@ class Base:
             dummy = cls(1)
         dummy.update(**dictionary)
         return (dummy)
+    
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+
