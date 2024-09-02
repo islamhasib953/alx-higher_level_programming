06c66f16d3ab9a2f81eb80c454be381959fbf55b
islamhasib953 <islamhasib953@example.com>
2024-09-02 15:25:32 +0300

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,10 @@
-# models/rectangle.py
-"""Rectangle
-"""
+#!/usr/bin/python3
+""" rectangle """
+
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Class Rectangle"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
@@ -16,12 +14,10 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -30,12 +26,10 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -44,12 +38,10 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """x getter"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """x setter"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
@@ -58,12 +50,10 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """y getter"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """y setter"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
